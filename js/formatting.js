e5e3de084537dce911d5964ea756619cdf100636
Pickle <20skibidiserb02@gmail.com>
2026-06-05 22:42:33 +0200

    Delete js directory

--- js/formatting.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-/* Formatting helpers */
-function formatDate(ts) { return new Date(ts).toLocaleDateString(); }
-function formatBytes(bytes) { return (bytes / 1024).toFixed(1) + ' KB'; }
-function truncate(str, n) { return str && str.length > n ? str.slice(0, n) + '...' : str; }
