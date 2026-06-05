57c75a0c4bdec278ffa2763bbf7e025a6a695171
Pickle <20skibidiserb02@gmail.com>
2026-06-06 00:29:18 +0200

    Delete js directory

--- js/sw-register.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('js/sw.js').then(function(r) {
-    console.log('SW registered:', r.scope);
-  }).catch(function(e) {
-    console.log('SW registration failed:', e);
-  });
-}
--- js/validation.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-/* Form validation helpers */
-function isEmpty(str) { return !str || str.trim() === ''; }
-function isValidUrl(str) { return /^https?:\/\//i.test(str) || /^mailto:/i.test(str); }
-function maxLen(str, n) { return typeof str === 'string' && str.length <= n; }
